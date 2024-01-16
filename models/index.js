bcca7bd3a34c42dcba51ff5dbe886e23af8649bb
mukhbitalya07@gmail.com <mukhbitalya07@gmail.com>
2024-01-16 19:06:53 +0700

    first commit

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const dbConfig = require("../config/db");
+const Sequelize = require("sequelize");
+const sequlize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorAlias: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
+});
+const db = {};
+db.Sequelize = Sequelize;
+db.sequlize = sequlize;
+
+// define mode
+db.quizzes = require("./quiz")(sequlize, Sequelize);
+module.exports = db;
